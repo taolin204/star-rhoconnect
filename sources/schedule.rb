d4cbfd4e0b53f07b7e568854666ae28e8284278f
TAO LIN <t4ol1n@gmail.com>
2013-03-15 12:00:35 +0800

    add source adatpr for schedule

---
+++ sources/schedule.rb
@@ -0,0 +1,86 @@
+class Schedule < SourceAdapter
+  def initialize(source) 
+    @base = 'http://192.168.2.22:3000'
+    super(source)
+  end
+ 
+  def login
+    # TODO: Login to your data source here if necessary
+    @token = Store.get_value("username:#{current_user.login}:token")
+    puts "token loaded for #{current_user.login}, token: #{@token}"
+  end
+ 
+  def query(params=nil)
+    # TODO: Query your backend data source and assign the records 
+    # to a nested hash structure called @result. For example:
+    # @result = { 
+    #   "1"=>{"name"=>"Acme", "industry"=>"Electronics"},
+    #   "2"=>{"name"=>"Best", "industry"=>"Software"}
+    # }
+    # raise SourceAdapterException.new("Please provide some code to read records from the backend data source")
+    puts "calling query #{@base}/schedules.json?auth_token=#{@token}"
+    rest_result = RestClient.get("#{@base}/schedules.json?auth_token=#{@token}").body
+    if rest_result.code != 200
+      raise SourceAdapterException.new("Error connecting!")
+    end
+    parsed = JSON.parse(rest_result)
+    puts "query results: #{parsed}"
+
+    @result={}
+    parsed.each do |item|
+      @result[item["id"].to_s] = {
+        :user_id        => item["user_id"],
+        :company_id     => item["company_id"],
+        :interviewee_id => item["interviewee_id"],
+        :schedule_time  => item["schedule_time"],
+        :status         => item["status"],
+        :remark         => item["remark"]
+      }
+    end if parsed
+  end
+ 
+  def sync
+    # Manipulate @result before it is saved, or save it 
+    # yourself using the Rhoconnect::Store interface.
+    # By default, super is called below which simply saves @result
+    super
+  end
+ 
+  def create(create_hash)
+    # TODO: Create a new record in your backend data source
+    puts "calling create #{@base}/schedules?auth_token=#{@token}"
+    res = RestClient.post("#{@base}/schedules?auth_token=#{@token}",{:schedule => create_hash, :content_type => :json, :accept => :json})
+    puts "create results: #{res}"
+    # TODO: Create a new record in your backend data source
+    # raise "Please provide some code to create a single record in the backend data source using the create_hash"
+    JSON.parse( res.body )["id"]
+  end
+ 
+  def update(update_hash)
+    # TODO: Update an existing record in your backend data source
+    obj_id = update_hash['id']
+    update_hash.delete('id')
+    puts "calling update #{@base}/schedules/#{obj_id}?auth_token=#{@token}"
+    res = RestClient.put("#{@base}/schedules/#{obj_id}?auth_token=#{@token}", {:schedule => update_hash, :content_type => :json, :accept => :json})
+    puts "update results: #{res}"
+    # raise "Please provide some code to update a single record in the backend data source using the update_hash"
+  end
+ 
+  def delete(delete_hash)
+    # TODO: write some code here if applicable
+    # be sure to have a hash key and value for "object"
+    # for now, we'll say that its OK to not have a delete operation
+    # raise "Please provide some code to delete a single object in the backend application using the object_id"
+    if delete_hash['id']
+      puts "calling delete"
+      puts "Calling destroy on #{@base}/schedules/#{delete_hash['id']}?auth_token=#{@token}"
+      res = RestClient.delete("#{@base}/schedules/#{delete_hash['id']}?auth_token=#{@token}")
+    else
+      puts "no delete_hash['id']"
+    end
+  end
+ 
+  def logoff
+    # TODO: Logout from the data source if necessary
+  end
+end
---
+++ spec/sources/schedule_spec.rb
@@ -0,0 +1,25 @@
+require File.join(File.dirname(__FILE__),'..','spec_helper')
+
+describe "Schedule" do
+  it_should_behave_like "SpecHelper" do
+    before(:each) do
+      setup_test_for Schedule,'testuser'
+    end
+
+    it "should process Schedule query" do
+      pending
+    end
+
+    it "should process Schedule create" do
+      pending
+    end
+
+    it "should process Schedule update" do
+      pending
+    end
+
+    it "should process Schedule delete" do
+      pending
+    end
+  end  
+end
